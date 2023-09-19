106930570c43bd6b452c54b9134d2e07ea19680f
Taku Fukada <naninunenor@gmail.com>
2023-09-19 10:21:35 +0900

    Code to BBox: バグ修正と浮動小数点誤差の軽減

--- japanese_grids/algorithms/utils/gridsquare_to_box.py
+++ japanese_grids/algorithms/utils/gridsquare_to_box.py
@@ -17,31 +17,35 @@ def grid_square_code_to_bbox(  # noqa: C901
     lat = int(code[:2])
     lng = int(code[2:4]) + 100
     if length == 4:
-        return (lng / 1.5, lat, lng + 1, (lat + 1) / 1.5)
+        return (lng, lat / 1.5, lng + 1, (lat + 1) / 1.5)
 
     lat += int(code[4]) * 0.125
     lng += int(code[5]) * 0.125
     if length == 6:
-        return (lng / 1.5, lat, lng + 0.125, (lat + 0.125) / 1.5)
+        return (lng, lat / 1.5, lng + 0.125, (lat + 0.125) / 1.5)
 
     lat10 = int(code[6]) * 0.125
     lng10 = int(code[7]) * 0.125
     if length == 8:
-        lat = (lat + lat10 / 10) / 1.5
-        lng = lng + lng10 / 10
-        return (lng, lat, lng + 0.0125, lat + 0.0125 / 1.5)
+        lat1 = lat + lat10 / 10
+        lng1 = lng + lng10 / 10
+        lat2 = lat + (lat10 + 0.125) / 10
+        lng2 = lng + (lng10 + 0.125) / 10
+        return (lng1, lat1 / 1.5, lng, lat / 1.5)
 
     a = code[8]
     if a not in _VALID_QUAD:
         return None
     if a == "3" or a == "4":
         lat10 += 0.0625
-    if a == "1" or a == "4":
+    if a == "2" or a == "4":
         lng10 += 0.0625
     if length == 9:
-        lat = (lat + lat10 / 10) / 1.5
-        lng = lng + lng10 / 10
-        return (lng, lat, lng + 0.00625, lat + 0.00625 / 1.5)
+        lat1 = lat + lat10 / 10
+        lng1 = lng + lng10 / 10
+        lat2 = lat + (lat10 + 0.0625) / 10
+        lng2 = lng + (lng10 + 0.0625) / 10
+        return (lng1, lat1 / 1.5, lng2, lat2 / 1.5)
 
     a = code[9]
     if a not in _VALID_QUAD:
@@ -51,9 +55,11 @@ def grid_square_code_to_bbox(  # noqa: C901
     if a == "2" or a == "4":
         lng10 += 0.03125
     if length == 10:
-        lat = (lat + lat10 / 10) / 1.5
-        lng = lng + lng10 / 10
-        return (lng, lat, lng + 0.003125, lat + 0.003125 / 1.5)
+        lat1 = lat + lat10 / 10
+        lng1 = lng + lng10 / 10
+        lat2 = lat + (lat10 + 0.03125) / 10
+        lng2 = lng + (lng10 + 0.03125) / 10
+        return (lng1, lat1 / 1.5, lng2, lat2 / 1.5)
 
     a = code[10]
     if a not in _VALID_QUAD:
@@ -62,13 +68,8 @@ def grid_square_code_to_bbox(  # noqa: C901
         lat10 += 0.015625
     if a == "2" or a == "4":
         lng10 += 0.015625
-    lat = (lat + lat10 / 10) / 1.5
-    lng = lng + lng10 / 10
-    return (lng, lat, lng + 0.0015625, lat + 0.0015625 / 1.5)
-
-
-if __name__ == "__main__":
-    latlng = grid_square_code_to_bbox("M55385272342")
-    print(latlng)
-    latlng = grid_square_code_to_bbox("M55385272342")
-    print(latlng)
+    lat1 = lat + lat10 / 10
+    lng1 = lng + lng10 / 10
+    lat2 = lat + (lat10 + 0.015625) / 10
+    lng2 = lng + (lng10 + 0.015625) / 10
+    return (lng1, lat1 / 1.5, lng2, lat2 / 1.5)
